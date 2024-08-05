9bd20369027c53fa8e8fde7d53a2017bfca0d320
jelloliu <jello.liu@yuanqu-tech.com>
2024-08-05 14:49:13 +0800

    feat: 解析命令行参数

--- packages/create-treact/src/index.ts
+++ packages/create-treact/src/index.ts
@@ -1,5 +1,9 @@
+import { parseCommandOptions } from './util';
+
 function __main() {
   console.log('执行脚手架 @dztony/create-treact 命令');
+  const options = parseCommandOptions();
+  console.log('获取的参数 - ', options);
 }
 
 __main();
---
+++ packages/create-treact/src/util.ts
@@ -0,0 +1,27 @@
+import { Command } from 'commander';
+import pkg from '../package.json';
+import pc from 'picocolors';
+
+enum EnumMode {
+  csr = 'csr',
+  ssr = 'ssr',
+}
+
+export function parseCommandOptions() {
+  const program = new Command();
+  program
+    .name(pkg.name)
+    .version(pkg.version)
+    .description(pkg.description)
+
+  program
+    .option('-n, --name', 'New project name, default name is treact-sample-<timestamp>', `treact-sample-${new Date().getTime()}`)
+    .option(
+      '-m, --mode',
+      `render mode, default value is ${pc.bgCyan(EnumMode.csr)}, option value is ${pc.cyan(EnumMode.csr)} or ${pc.cyan(EnumMode.ssr)}`,
+      EnumMode.csr,
+      )
+
+  program.parse();
+  return program.opts();
+}
