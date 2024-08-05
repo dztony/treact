9e880f4a786e452ad4d41d304c986976c07539bb
jelloliu <jello.liu@yuanqu-tech.com>
2024-08-05 16:19:48 +0800

    feat: 增加 logo 打印

--- packages/create-treact/src/index.ts
+++ packages/create-treact/src/index.ts
@@ -1,17 +1,23 @@
-import { chooseTemplate, EnumMode, parseCommandOptions } from './util';
+import { chooseTemplate, copyTemplate, EnumMode, parseCommandOptions, printSuffixInfo } from './util';
 
 import pc from 'picocolors';
+import path from 'node:path';
+import process from 'node:process';
 
 function __main() {
-  console.log('执行脚手架 @dztony/create-treact 命令 - start');
+  // 解析入参
   const options = parseCommandOptions();
   if (!Object.values(EnumMode).includes(options.mode)) {
     throw Error(pc.red(`unknown render mode ${pc.cyan(options.mode)}, expected ${pc.cyan(EnumMode.csr)} or ${pc.cyan(EnumMode.ssr)}`));
   }
-  const targetTemplatePath = chooseTemplate(options.mode);
 
-  console.log('目标模板  - ', targetTemplatePath);
-  console.log('执行脚手架 @dztony/create-treact 命令 - end');
+  // 获取模板并复制
+  const template = chooseTemplate(options.mode);
+  const distTemplatePath = path.join(process.cwd(), options.name);
+  copyTemplate(template.path, distTemplatePath);
+
+  // 打印相关信息
+  printSuffixInfo();
 }
 
 __main();
--- packages/create-treact/src/util.ts
+++ packages/create-treact/src/util.ts
@@ -4,6 +4,8 @@ import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
 
+import figlet from 'figlet';
+
 import pkg from '../package.json';
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
@@ -16,6 +18,11 @@ export enum EnumMode {
 type IOptions = {
   name: string;
   mode: EnumMode;
+};
+
+type ITemplate = {
+  name: string;
+  path: string;
 };
 
 export function parseCommandOptions(): IOptions {
@@ -37,7 +44,7 @@ export function parseCommandOptions(): IOptions {
   return program.opts() as IOptions;
 }
 
-export function chooseTemplate(mode: EnumMode) {
+export function chooseTemplate(mode: EnumMode): ITemplate {
   const expectedTemplate = `template-react-${mode}`;
 
   const templateNameList = fs.readdirSync(path.join(__dirname, '../template'));
@@ -45,7 +52,28 @@ export function chooseTemplate(mode: EnumMode) {
     throw Error(`template ${pc.cyan(expectedTemplate)} not found, please submit a issue to ${pc.blue('https://github.com/dztony/treact')}`);
   }
 
-  return path.join(__dirname, '../template', expectedTemplate);
+  return {
+    name: expectedTemplate,
+    path: path.join(__dirname, '../template', expectedTemplate),
+  };
 }
 
+export function copyTemplate(from: string, to: string) {
+  // const sourceFiles = fs.readdirSync(from, { encoding: 'utf8' });
+  // console.log('sourceFiles', sourceFiles);
+  // console.log(to);
 
+  // TODO：复制前需要校验目标文件夹是否存在
+  // 1. 如果存在，则需要用户决定是否覆盖
+  //    如果用户选择 否，则需要停止创建
+  //    如果用户选择 是，则需要先删除掉目标文件夹，再复制
+  // 2. 如果不存在，则直接复制
+
+  fs.cpSync(from, to, { recursive: true });
+}
+
+export function printSuffixInfo() {
+
+  console.log(pc.green(figlet.textSync('T-react')));
+  console.log(pc.gray('create template success!!'));
+}
